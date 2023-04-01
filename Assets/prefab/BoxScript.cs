d32a87d77a6d1491b1b29048d7f1a582f866ae3f
mir4H <mira.m.hamalainen@gmail.com>
2023-04-01 15:31:58 +0300

    Destroy old box after 5seks

--- Assets/prefab/BoxScript.cs
+++ Assets/prefab/BoxScript.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxScript : MonoBehaviour
+public class BoxScript : MonoBehaviour  
 {
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("backwall"))
         {
             EventManager.OnBoxStucked();
-            Debug.Log("Do something else here");
+            Destroy(gameObject, 5f); 
         }
     }
 }
