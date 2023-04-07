581e85c099c1686cf00876f6be52ce26aa8ec91b
ojalaija <77692985+ojalaija@users.noreply.github.com>
2023-04-08 00:10:13 +0300

    InterAct-scene added, work in process

---
+++ Assets/Scripts/InteractionSystem/Chest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _prompt;
+
+    public string InteractionPrompt => _prompt;
+
+    public bool Interact(Interactor interactor)
+    {
+        //Tähän  mitä se interaktio tekee!!
+
+        Debug.Log("Opening Chest!");
+        return true;
+    }
+}
