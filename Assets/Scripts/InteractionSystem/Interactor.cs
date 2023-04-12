2dcfb076d2fecc64e49ba6f9be8b9accd6824417
ojalaija <77692985+ojalaija@users.noreply.github.com>
2023-04-12 14:33:49 +0300

    Added Interact to InputSystem, started MainMenu

--- Assets/Scripts/InteractionSystem/Interactor.cs
+++ Assets/Scripts/InteractionSystem/Interactor.cs
@@ -1,45 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Interactor : MonoBehaviour
 {
-    [SerializeField] private Transform _interactionPoint;
-    [SerializeField] private float _interactionPointRadius = 0.25f;
-    [SerializeField] private LayerMask _interactableMask;
-    [SerializeField] private InteractionPromptUI _interactionPromptUI;
+    [SerializeField] private Transform interactionPoint;
+    [SerializeField] private float interactionPointRadius = 0.25f;
+    [SerializeField] private LayerMask interactableMask;
+    [SerializeField] private InteractionPromptUI interactionPromptUI;
+    [SerializeField] InputActionReference interactionInput;
 
-    private readonly Collider[] _colliders = new Collider[3];
-    [SerializeField] private int _numFound;
+    private readonly Collider[] colliders = new Collider[3];
+    [SerializeField] private int numFound;
 
-    private IInteractable _interactable;
+    private IInteractable interactable;
 
     private void Update()
     {
-        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
+        numFound = Physics.OverlapSphereNonAlloc(interactionPoint.position, interactionPointRadius, colliders, interactableMask);
 
-        if (_numFound > 0)
+        if (numFound > 0)
         {
-            _interactable = _colliders[0].GetComponent<IInteractable>();
+            interactable = colliders[0].GetComponent<IInteractable>();
 
-            if (_interactable != null)
+            if (interactable != null)
             {
-                if (!_interactionPromptUI.IsDisplayed) _interactionPromptUI.SetUp(_interactable.InteractionPrompt);
-
-                if (Input.GetKeyDown(KeyCode.E)) _interactable.Interact(this);
+                if (!interactionPromptUI.IsDisplayed) interactionPromptUI.SetUp(interactable.InteractionPrompt);
+                interactionInput.action.performed += DoInteraction;
             } 
         }
 
         else
         {
-            if (_interactable != null) _interactable = null;
-            if (_interactionPromptUI.IsDisplayed) _interactionPromptUI.Close();
+            if (interactable != null) interactable = null;
+            if (interactionPromptUI.IsDisplayed) interactionPromptUI.Close();
         }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+        Gizmos.DrawWireSphere(interactionPoint.position, interactionPointRadius);
+    }
+
+    private void DoInteraction(InputAction.CallbackContext obj)
+    {
+        interactable.Interact(this);
     }
 }
