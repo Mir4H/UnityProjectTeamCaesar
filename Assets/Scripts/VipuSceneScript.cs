10cb5a303d0422a37a0bd7174b6b8f64e9872be2
nina20126 <nina20126@student.hamk.fi>
2023-04-27 00:38:59 +0300

    script for switches

---
+++ Assets/Scripts/VipuSceneScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VipuSceneScript : MonoBehaviour
+{
+    [SerializeField] private GameObject switch1;
+    [SerializeField] private GameObject switch2;
+    [SerializeField] private GameObject switch3;
+    [SerializeField] private GameObject switch4;
+    [SerializeField] private GameObject switch5;
+    [SerializeField] private GameObject switch6;
+
+    [SerializeField] private GameObject fence1;
+    [SerializeField] private GameObject fence2;
+    [SerializeField] private GameObject fence3;
+    [SerializeField] private GameObject fence4;
+
+    [SerializeField] private GameObject goalDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Up: 315, Down: 45
+    private void SwitchCheck()
+    {
+        if (switch1.transform.eulerAngles.x == 45 &&
+            switch2.transform.eulerAngles.x == 315 &&
+            switch3.transform.eulerAngles.x == 315 &&
+            switch4.transform.eulerAngles.x == 315 &&
+            switch5.transform.eulerAngles.x == 45 &&
+            switch6.transform.eulerAngles.x == 315)
+        {
+            Debug.Log("thrones right");
+
+            GameObject.Destroy(fence1);
+            GameObject.Destroy(fence2);
+            GameObject.Destroy(fence3);
+            GameObject.Destroy(fence4);
+        }
+    }
+}
